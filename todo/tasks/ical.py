0a2cf1091979082debedcec888cedcec24a444da
charlottemaherr <charlotte.maher@student.uq.edu.au>
2026-04-24 21:51:32 +1000

    Deploy with SQS

--- todo/tasks/ical.py
+++ todo/tasks/ical.py
@@ -1,4 +1,7 @@
 import os
+import icalendar
+import time
+import datetime
 
 from celery import Celery
 
@@ -9,4 +12,20 @@ celery.conf.task_default_queue = os.environ.get("CELERY_DEFAULT_QUEUE", "ical")
 
 @celery.task(name="ical")
 def create_ical(tasks):
-    return "Hello World"
+    cal = icalendar.Calendar()
+    cal.add("prodid", "-//Taskoverflow Calendar//mxm.dk//")
+    cal.add("version", "2.0")
+
+    time.sleep(50)
+
+    for task in tasks:
+        event = icalendar.Event()
+
+        event.add("uid", task["id"])
+        event.add("summary", task["title"])
+        event.add("description", task["description"])
+        event.add("dtstart", datetime.datetime.strptime(task["deadline_at"], "%Y-%m-%dT%H:%M:%S"))
+
+        cal.add_component(event)
+
+    return cal.to_ical().decode("utf-8")
